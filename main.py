387dc0b2085fa9142e58633df59b9cbdf2ee5dac
Black <blackbomba32@hotmail.fr>
2022-05-01 22:41:02 +0200

    Added arguments

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import sys
 import socket
-
+import argparse
 
 HOST = "192.168.119.141"
 PORT = 3000
 
 
-def test():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        result = s.connect_ex((HOST, PORT))
-        if result == 0:
-            print("Port open")
-        else:
-            print("Port closed")
+def start(args):
+    if args is None:
+        raise Exception('Issue with args !')
+    else:
+        print("Start...")
 
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-h', '--host',
+                        type=str,
+                        help='Specify the target host')
+    parser.add_argument('-p', '--port',
+                        type=int,
+                        default=80,
+                        help='Specify the port of the target host. 80 by default')
+    args = parser.parse_args()
+    sys.exit(start(args))
+    
 
 if __name__ == '__main__':
-    sys.exit(test())
-
+    sys.exit()
